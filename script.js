1cb8f3d454e186c592224144a387544c7d9c264b
William H.W <wiliamwijaya1985@gmail.com>
2020-11-09 17:36:12 +0700

    Add auto-close navbar on mobile view

--- script.js
+++ script.js
@@ -20,13 +20,35 @@ function carousel() {
 
 function dropDown() {
     let x = document.getElementById('myTopnav');
-    console.log(x.className);
+    // console.log(x.className);
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
 }
+
+// Navbar auto-close
+(function() {
+    let width = window.innerWidth;
+    // console.log(width);
+
+    if (width <= 554) {
+        let x = document.getElementById('myTopnav');
+
+        // Get click position
+        window.onclick = function(e) {
+            e = e || window.event;
+            let target = e.target;
+            let text = target.className;
+            // console.log(text);
+
+            if (text !== 'fa fa-bars' && x.className === 'topnav responsive') {
+                x.className = 'topnav';
+            }
+        };
+    }
+})();
 
 // Scrollspy
 (function() {
@@ -40,12 +62,16 @@ function dropDown() {
     Array.prototype.forEach.call(section, function(e) {
         sections[e.id] = e.offsetTop;
     });
+    // console.log(sections);
 
     window.onscroll = function() {
         let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+        // console.log(scrollPosition + 140);
 
         for (i in sections) {
             if (scrollPosition + 140 >= sections[i]) {
+                // console.log(i, sections[i]);
+
                 document.querySelector('.current').setAttribute('class', 'link');
                 if (i == "main-title") {
                     target = document.querySelectorAll('a[href*=' + i + ']')[1];
